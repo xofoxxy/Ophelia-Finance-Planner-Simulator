094e0c5ff315eb3a87401f0fa64d3a101bc26151
xofoxxy <sethleavitt8@gmail.com>
2025-06-18 11:03:15 -0600

    Added handling for superAccount nesting.

--- superAccount.py
+++ superAccount.py
@@ -14,3 +14,7 @@ class SuperAccount:
             if isinstance(single, account.Account):
                 children.append(single)
                 print(f"{single.name}, is a valid account, {len(children) - 1}th child of {name}")
+
+            if isinstance(single, SuperAccount):
+                children.append(single)
+                print(f"{single.name}, is a valid superAccount, {len(children) - 1}the child of {name}")
