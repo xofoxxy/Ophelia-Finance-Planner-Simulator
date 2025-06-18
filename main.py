4c3d997b267b5b76b1d90703a254a5dbd4331e96
Xofox <sethleavitt8@gmail.com>
2025-06-17 19:06:00 -0600

    Initial Commit

--- main.py
+++ main.py
@@ -10,10 +10,3 @@ if __name__ == '__main__':
     # bridgette_account = account.Account("Bridgette", balance=10000, appreciation=1.06, costs=6000, income=bridge_income)
     # bridgette_account.simple_project_balance(10)
     # bridgette_account.graph_everything()
-
-
-    def kids_costs(year):
-        return 10000
-    kids_cost = account.Account("Kids", balance=0, costs=kids_costs)
-    kids_cost.simple_project_balance(10)
-    kids_cost.graph_everything()
